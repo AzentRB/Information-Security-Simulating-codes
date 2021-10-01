12a3e1f5ca769822aa7b5f1762acbe2b8f3d02b5
AzentRB <90961127+AzentRB@users.noreply.github.com>
2021-10-01 11:57:07 +0530

    Added required codes
    Codes for the repository

---
+++ SDES.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+string enc(string key,string plaintext,int round){
+	string Li,Ri,Ln,Rn,K,f;
+	K=keyer(key,round);Li.append(plaintext,0,6);
+	Ri.append(plaintext,6,6);Ln=Ri;
+	f.append(hehe(Ri,K));
+	Rn.append(f);Rn=XOR(Li,f);
+	return (Ln+Rn);
+}
+string dec(string key,string ciphertext,int round){
+	string Li,Ri,Ln,Rn,K,f;
+	K=keyer(key,round);Li.append(ciphertext,0,6);
+	Ri.append(ciphertext,6,6);Rn=Li;
+	f.append(hehe(Rn,K));Ln.append(f);
+	Ln=XOR(Ri,f);
+	return (Ln+Rn);
+}
+string keyer(string key,int round){
+	string K;
+	if(round==1) K.append(key,0,8);
+	else if(round==2) K.append(key,1,8);
+	else if(round==3) K.append(key,2,7),K.append(key,0,1);
+	else if(round==4) K.append(key,3,6),K.append(key,0,2);
+	return K;
+}
+string hehe(string R,string K){
+	char tmp;
+	string s1,s2;
+	R.append(R,4,2);
+	tmp=R[3];R[5]=R[2];R[4]=tmp;
+	R[3]=R[2];R[2]=tmp;R=XOR(R,K);
+	s1.append(R,0,4);s2.append(R,4,4);
+	return bx1(s1)+bx2(s2);
+}
+string XOR(string x,string y){
+	for(int i=0;i<x.length();i++)
+		if(x[i]==y[i]) x[i]='0';
+		else if(x[i]!=y[i]) x[i]='1';
+	return x;
+}
+string bx1(string s1){
+	string row1[8]={"101","010","001","110","011","100","111","000"};
+	string row2[8]={"001","100","110","010","000","111","101","011"};
+	int column=0;
+	if(s1[0]=='0') {
+		if(s1[1]=='1') column += 4;
+		if(s1[2]=='1') column += 2;
+		if(s1[3]=='1') column += 1;
+		return row1[column];
+	}
+	else if(s1[0]=='1'){
+        if(s1[1]=='1') column += 4;
+		if(s1[2]=='1') column += 2;
+		if(s1[3]=='1') column += 1;
+		return row2[column];
+	}
+	else return "ERROR";
+}
+string bx2(string s2){
+	string row1[8]={"100","000","110","101","111","001","011","010"};
+	string row2[8]={"101","011","000","111","110","010","001","100"};
+	int column=0;
+	if(s2[0]=='0'){
+		if(s2[1]=='1') column += 4;
+		if(s2[2]=='1') column += 2;
+		if(s2[3]=='1') column += 1;
+		return row1[column];
+	}
+	else if(s2[0]=='1') {
+		if(s2[1]=='1') column+=4;
+		if(s2[2]=='1') column += 2;
+		if(s2[3]=='1') column += 1;
+		return row2[column];
+	}
+	else return "ERROR";
+}
+string randomString(int length){
+	srand(time(0));
+	int randomNumber;
+	string randomBit,randomString;
+	for(int k=0; k < length; k++){
+		randomNumber=rand() % 2;
+		if(randomNumber==0) randomBit="0";
+		else if(randomNumber==1) randomBit="1";
+		randomString.append(randomBit);
+	}
+	return randomString;
+}
+void solve(){
+    string plaintext=randomString(12);
+	string key=randomString(9),ciphertext,decryption;
+	cout << "Original Plaintext: " << plaintext << endl << "Key: " << key << endl << "Encryption: " << endl;
+	for(int i=0; i < numrounds; i++){
+		plaintext=enc(key,plaintext,i+1);
+		cout << "C (after round " << i+1 << "): " << plaintext << endl;
+		rounds=i+1;
+	}
+	for(int j=numrounds; j > 0; j--) {
+		decryption=dec(key,decryption,j);
+		if(j != 1) cout << "C(after round " << j << "): " << decryption << endl;
+		else if(j==1) cout << "Plaintext after:  " << decryption << endl << endl;
+	}
+}
+int main(){
+    fast_cin();
+    ll t=1;
+    //cin >> t;
+    for(int it=1;it<=t;it++) {
+        //cout << "Case #" << it+1 << ": ";
+        solve();
+    }
+    return 0;
+}
---
+++ SHA1.cpp
@@ -0,0 +1,95 @@
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+vector<int> X; 
+int W[80],A,B,C,D,E,A1,B1,C1,D1,E1,lol;
+void printX() { 
+    for(int i=0;i<X.size();i++) {
+        printf("%02x",X[i]);
+        if((i+1)%16==0) printf("\n");
+    }
+}
+int S(unsigned int x, int n) { return x>>(32-n)|(x << n);}
+void append(string m) {  
+    lol=(m.size()+8)/64+1;X.resize(lol*64);
+    X[i++]=0x80;while(i<X.size()-8) X[i]=0,i++;
+    long long int a=m.size() * 8;
+    for (i=X.size() - 1;i >= X.size() - 8;i--) X[i] =a%256,a/=256;
+}
+void setW(vector<int> m, int n) { 
+    n *= 64;
+    for(int i=16;i<80;i++) W[i]=S(W[i-16]^W[i-14]^W[i-8]^W[i-3],1);
+}
+int ft(int t) {
+    if(t<20) return (B&C)|((~B)&D);
+    else if(t<40) return B^C^D;
+    else if(t<60) return (B&C)|(B&D)|(C&D);
+    else return B^C^D;
+}
+int Kt(int t) { 
+    if(t<20) return 0x5a827999;
+    else if(t<40) return 0x6ed9eba1;
+    else if(t<60) return 0x8f1bbcdc;
+    else return 0xca62c1d6;
+}
+void sha1(string text) {
+    A1=A=0x67452301,B1=B=0xefcdab89,C1=C=0x98badcfe,D1=D=0x10325476,E1=E=0xc3d2e1f0;
+    append(text);printX();
+    int temp;
+    for(int i=0;i<lol;i++) {
+        setW(X,i);
+        A1=A=A+A1,B1=B=B+B1,C1=C=C+C1,D1=D=D+D1,E1=E=E+E1;
+    }
+    printf("%08x%08x%08x%08x%08x\n\n", A1, B1, C1, D1, E1);
+}
+void solve(){
+    string text="malhr";
+    sha1(text);
+}
+int main(){
+    fast_cin();
+    ll t=1;
+    //cin >> t;
+    for(int it=1;it<=t;it++) {
+        //cout << "Case #" << it+1 << ": ";
+        solve();
+    }
+    return 0;
+}
